ace8cacade7ca4abb20bc8b657eca2187bdfd3a4
Georges <169011770+KGeox@users.noreply.github.com>
2026-04-05 13:24:26 +0100

    new day

--- BookSocialMedia/settings.py
+++ BookSocialMedia/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-i6u%19ufmo4_&#u9l@wy!%7_zj7^r-zi(f3ea=0u!m4t@v!e!p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
